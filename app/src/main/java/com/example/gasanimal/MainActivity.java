6edbe81285d931bd588ecf1e1c36cf7b32afc86c
Nurbek03 <nurbekabdilamit@gmail.com>
2021-01-12 14:28:17 +0300

    hw1

--- app/src/main/java/com/example/gasanimal/MainActivity.java
+++ app/src/main/java/com/example/gasanimal/MainActivity.java
@@ -2,16 +2,36 @@ package com.example.gasanimal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView t1, t2;
+    private EditText ed_login, ed_parol;
+    private Button btn_list2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ed_login = findViewById(R.id.ed_login);
+        ed_parol = findViewById(R.id.ed_parol);
+        btn_list2 = findViewById(R.id.btn_list2);
 
-        
+
+
+
+    }
+
+    public void setClik(View view) {
+       Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+       startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/gasanimal/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.gasanimal;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one);
+
+
+    }
+}
